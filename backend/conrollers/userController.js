17546100d7f972990825e5a88e65dca1e1a7bae1
pratapsinghthakur45 <pratapsinghthakurr45@gmail.com>
2026-08-21 14:58:01 +0530

    login route added successfully

--- backend/conrollers/userController.js
+++ backend/conrollers/userController.js
@@ -3,6 +3,8 @@ import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import bcryptjs from 'bcryptjs';
 
+
+//register api is
 export const register = async (req,res) => {
           try {
             const data = req.body;
@@ -30,5 +32,39 @@ export const register = async (req,res) => {
             return res.status(201).json({message:"User register Successfully:",response:response,token:token});
           } catch (error) {
               console.log(error);
+              res.status(500).json({message:"Internal Server Error:"});
           }
+}
+
+export const login = async (req,res) =>{
+    try {
+        const {email,password} = req.body;
+
+        const user = await User.findOne({email:email});
+
+        if(!user){
+            return res.status(404).json({message:"User Not Found:"});
+        }
+
+        const isMatch = await bcryptjs.compare(password,user.password);
+        if(!isMatch){
+            return res.status(401).json({message:"Invalid Password"});
+        }
+
+        const token = jwt.sign(
+            {
+                id:user._id
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn:"1h"
+            }
+        );
+
+        return res.status(200).json({message:"User Login Successfully:",token:token});
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"Internal Server Error:"});
+    }
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import jwtAuth from "../middlewares/jwtAuth.js";
-import { register } from "../conrollers/userController.js";
+import { register,login } from "../conrollers/userController.js";
 
 const router = express.Router();
 
 router.post('/register',register);
+
+router.post('/login',login);
 
 export default router;
